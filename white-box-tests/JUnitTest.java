cfa60676e8ea93adf2dff3bc44eceaf7b50edf97
Brian Lee <brianlee@Brians-MacBook-Air-2.local>
2018-04-02 01:42:08 -0700

    added test cases

--- white-box-tests/JUnitTest.java
+++ white-box-tests/JUnitTest.java
@@ -20,17 +20,36 @@ public class JUnitTest {
 		CollageBuilder collageBuilder = new CollageBuilder(800, 600);
 		assertNotNull(collageBuilder);
 	}
+
 	
 	// Test for buildCollage method in CollageBuilder class
 	@Test
 	public void testBuildCollage() {
 		CollageBuilder collageBuilder = new CollageBuilder(800, 600);
 		ImageSourcer s = new ImageSourcer("cat", 30);
-		BufferedImage collage = collageBuilder.buildCollage(s, "S");
+		BufferedImage collage = collageBuilder.buildCollage(s, "SH PE");
 		assertNotNull(collage);
+		System.out.println(collage.getWidth());
+		System.out.println(collage.getHeight());
 		assertTrue(collage.getWidth() == 800);
 		assertTrue(collage.getHeight() == 600);
 	}
+	
+	// Test for buildCollage method in CollageBuilder class
+	@Test
+	public void testBuildCollageBrowser() {
+		// Using browser width and height 1440 and 900
+		CollageBuilder collageBuilder = new CollageBuilder(1440, 900);
+		ImageSourcer s = new ImageSourcer("cat", 30);
+		BufferedImage collage = collageBuilder.buildCollage(s, "S");
+		assertNotNull(collage);
+		// Max of 0.7*1440 = 1007 and 800
+		assertTrue(collage.getWidth() == 1007);
+		// Max of 0.5*900 = 450 and 600
+		assertTrue(collage.getHeight() == 600);
+	}
+	
+	
 	
 	// Test for rotateImage method in CollageBuilder class
 	@Test
