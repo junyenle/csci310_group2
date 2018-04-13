f75764e782a19c21c3fb4ae1ddaf15d39e15477c
Brian Lee <lee789@usc.edu>
2018-04-13 14:03:09 -0700

    Add files via upload
    functionality for save collage

--- prototypes/source_classes/CollageManager.java
+++ prototypes/source_classes/CollageManager.java
@@ -7,12 +7,67 @@ public class CollageManager {
 	private Vector<String> collageTitles = new Vector<String>();
 	// Vector holding all Collages in the session
 	private Vector<BufferedImage> collages = new Vector<BufferedImage>();
+	// Vector holding all saved collages
+	private Vector<BufferedImage> savedCollages = new Vector<BufferedImage>();
+	// Vector holding all saved collage titles
+	private Vector<String> savedCollageTitles = new Vector<String>();
 	
 	// Insert a collage and title into both vectors
 	public void insertCollage(String title, BufferedImage collage) {
 		collageTitles.add(title);
 		collages.add(collage);
 	}
+	
+	public static boolean compareImages(BufferedImage imgA, BufferedImage imgB) 
+	{
+		// check image sizes
+		if (imgA.getWidth() != imgB.getWidth() || imgA.getHeight() != imgB.getHeight()) 
+		{
+		  return false;
+		}
+		
+		int width  = imgA.getWidth();
+		int height = imgA.getHeight();
+
+		// for each pixel
+		for (int i = 0; i < height; i++) 
+		{
+		  for (int j = 0; j < width; j++) 
+		  {
+		    // compare pixels
+		    if (imgA.getRGB(j, i) != imgB.getRGB(j, i)) 
+		    {
+		      return false;
+		    }
+		  }
+		}
+		
+		return true;
+	}
+	
+	// save a collage - returns false if invalid save, returns true otherwise
+	public boolean saveCollage()
+	{
+		if(collages.isEmpty())
+		{
+			return false;
+		}
+		// first check to make sure the displayed collage is not already saved
+		BufferedImage displayedCollage = collages.get(collages.size()-1);
+		for(int i = 0; i < savedCollages.size(); i++)
+		{
+			BufferedImage oldCollage = savedCollages.get(i);
+			if(CollageManager.compareImages(oldCollage, displayedCollage))
+			{
+				return false;
+			}
+		}
+		// no match found
+		savedCollages.add(displayedCollage);
+		savedCollageTitles.add(collageTitles.get(collageTitles.size()-1));
+		return true;
+	}
+	
 	// Getter
 	public Vector<String> getCollageTitles() {
 		return collageTitles;
@@ -20,5 +75,15 @@ public class CollageManager {
 	// Getter
 	public Vector<BufferedImage> getCollages() {
 		return collages;
+	}
+
+	// Getter
+	public Vector<BufferedImage> getSavedCollages() {
+		return savedCollages;
+	}
+
+	// Getter
+	public Vector<String> getSavedCollageTitles() {
+		return savedCollageTitles;
 	}	
 }
